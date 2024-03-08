27fce06ce1390fbf2cdd0a80f6902753ca480267
Dimitris Angos <dimitrisaggos@gmail.com>
2024-03-08 08:53:53 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
